b57eb9ee62a2b9cad7791180910db1104abfbb77
AKestell <>
2023-10-05 20:12:07 +0000

    Added event listeners to js

---
+++ assets/js/script.js
@@ -0,0 +1,9 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const choices = document.querySelectorAll(".choice");
+    const userScore = document.getElementById("user.score");
+    const computerScore = document.getElementById("computer-score");
+    const actionMessage = document.getElementById("action-message");
+
+
+    
+})
