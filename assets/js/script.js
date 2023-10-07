d9fc8fa5ab2529a07084d2ad18d08b147b3c61dc
AKestell <>
2023-10-07 16:50:51 +0000

    Updated code in 'updateScore' function and fixed spelling errors

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,61 +1,60 @@
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", function () {
     const choices = document.querySelectorAll(".choice");
-    const userScore = document.getElementById("user.score");
+    const userScore = document.getElementById("user-score");
     const computerScore = document.getElementById("computer-score");
     const actionMessage = document.getElementById("action-message");
 
     let userScoreNum = 0;
     let computerScoreNum = 0;
 
+    function getComputerChoice() {
+        const choices = ["rock", "paper", "scissors"];
+        const randNum = Math.floor(Math.random() * 3);
+        return choices[randNum];
+    }
+
+    function whoWins(userChoice, computerChoice) {
+        if (userChoice === computerChoice) {
+            return `${userChoice} equals ${computerChoice}. It's a draw!`;
+        } else if (
+            (userChoice === "Rock" && computerChoice === "Scissors") ||
+            (userChoice === "Scissors" && computerChoice === "Paper") ||
+            (userChoice === "Paper" && computerChoice === "Rock")
+
+        ) {
+            return `${userChoice} beats ${computerChoice}. You win!`;
+        } else {
+            return `${userChoice} loses to ${computerChoice}. You lose!`;
+        }
+    }
+
+    function updateScore(userChoice, computerChoice, result) {
+        if (
+            result.includes("You win!")
+        ) {
+            userScoreNum++;
+        } else if (
+            result.includes("You lose!")
+        ) {
+            computerScoreNum++;
+        }
+
+        userScore.textContent = userScoreNum;
+        computerScore.textContent = computerScoreNum;
+        actionMessage.textContent = `${userChoice} vs ${computerChoice}. ${result}`;
+    }
+
     function startGame() {
-        choices.forEach ((choice) => {
+        choices.forEach((choice) => {
             choice.addEventListener("click", function () {
                 const userChoice = this.id;
                 const computerChoice = getComputerChoice();
-                const result = determineWinner(userChoice, computerChoice);
+                const result = whoWins(userChoice, computerChoice);
                 updateScore(userChoice, computerChoice, result);
             });
         });
     }
 
-    function getComputerChoice() {
-        const choices = ["rock", "paper", "scissors"];
-        const randNum = Math.floor(Math.random() *3) + 1;
-        return choices[randNum]
-    }
+    startGame();
 
-    function whoWins(userChoice, computerChoice) {
-        if (userChoice === computerChoice) {
-            return `${convertToWord(userChoice)} equals ${convertToWord(computerChoice)}. It's a draw!`; 
-        } else if (
-            (userChoice === "rock" && computerChoice === "scissors") || 
-            (userChoice === "scissors" && computerChoice === "rock") ||
-            (userChoice === "paper" && computerChoice === "rock") 
-
-        ) {
-            return `${convertToWord(userChoice)} beats ${convertToWord(computerChoice)}. You win!`;
-        } else {
-            return `${convertToWord(userChoice)} loses to ${convertToWord(computerChoice)}. You lose!`;
-        }
-    }
-
-        function updateScore(userChoice, computerChoice, result) {
-            if (
-                (result === "Rock beats Scissors. You win!") ||
-                (result === "Scissors beats Paper. You win!") ||
-                (result === "Paper beats Rock You. win!") 
-                ) {
-                userScoreNum++;
-            } else if (
-                (result === "Rock loses to Paper. You lose!") ||
-                (result === "Paper loses to Scissors. You lose!") ||
-                (result === "Scissors loses to Rock. You lose!")      
-                ) {
-                computerScoreNum++;    
-                }
-        }
-    
-    }
-
-})
-
+});
