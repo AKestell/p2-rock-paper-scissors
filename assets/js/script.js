0efc20767de2be1815ec81c675e433a035394280
AKestell <>
2023-10-08 12:11:28 +0000

    Added 'Rules of the Game' section to the top of the page

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,69 +1,61 @@
 document.addEventListener("DOMContentLoaded", function () {
-    const choices = document.querySelectorAll(".choice");
-    const userScore = document.getElementById("user-score");
-    const computerScore = document.getElementById("computer-score");
-    const actionMessage = document.getElementById("action-message");
-    const resultMessage = document.querySelector('.result');
+  const choices = document.querySelectorAll(".choice");
+  const userScore = document.getElementById("user-score");
+  const computerScore = document.getElementById("computer-score");
+  const actionMessage = document.getElementById("action-message");
+  const resultMessage = document.querySelector(".result");
 
+  let userScoreNum = 0;
+  let computerScoreNum = 0;
 
-    let userScoreNum = 0;
-    let computerScoreNum = 0;
+  // Function to determine the Computers choice.
+  function getComputerChoice() {
+    const choices = ["rock", "paper", "scissors"];
+    const randNum = Math.floor(Math.random() * 3);
+    return choices[randNum];
+  }
 
+  // Function to see which player wins the game.
+  function whoWins(userChoice, computerChoice) {
+    if (userChoice === computerChoice) {
+      return `${userChoice} equals ${computerChoice}. It's a draw!`;
+    } else if (
+      (userChoice === "rock" && computerChoice === "scissors") ||
+      (userChoice === "scissors" && computerChoice === "paper") ||
+      (userChoice === "paper" && computerChoice === "rock")
+    ) {
+      return `${userChoice} beats ${computerChoice}. You win!`;
+    } else {
+      return `${userChoice} loses to ${computerChoice}. You lose!`;
+    }
+  }
 
-    // Function to determine the Computers choice.
-    function getComputerChoice() {
-        const choices = ["rock", "paper", "scissors"];
-        const randNum = Math.floor(Math.random() * 3);
-        return choices[randNum];
+  //Function to update and display the score of the game.
+  function updateScore(userChoice, computerChoice, result) {
+    if (result.includes("You win!")) {
+      userScoreNum++;
+    } else if (result.includes("You lose!")) {
+      computerScoreNum++;
     }
 
-    // Function to see which player wins the game. 
-    function whoWins(userChoice, computerChoice) {
-        if (userChoice === computerChoice) {
-            return `${userChoice} equals ${computerChoice}. It's a draw!`;
-        } else if (
-            (userChoice === "rock" && computerChoice === "scissors") ||
-            (userChoice === "scissors" && computerChoice === "paper") ||
-            (userChoice === "paper" && computerChoice === "rock")
+    userScore.textContent = userScoreNum;
+    computerScore.textContent = computerScoreNum;
+    result.textContent = result;
+    actionMessage.textContent = `${userChoice} vs ${computerChoice}. ${result}`;
+  }
 
-        ) {
-            return `${userChoice} beats ${computerChoice}. You win!`;
-        } else {
-            return `${userChoice} loses to ${computerChoice}. You lose!`;
-        }
-    }
+  // Event listener for the players choices.
+  function startGame() {
+    choices.forEach((choice) => {
+      choice.addEventListener("click", function () {
+        const userChoice = this.id;
+        const computerChoice = getComputerChoice();
+        const result = whoWins(userChoice, computerChoice);
+        updateScore(userChoice, computerChoice, result);
+      });
+    });
+  }
 
-    //Function to update and display the score of the game. 
-    function updateScore(userChoice, computerChoice, result) {
-        if (
-            result.includes("You win!")
-        ) {
-            userScoreNum++;
-        } else if (
-            result.includes("You lose!")
-        ) {
-            computerScoreNum++;
-        }
-
-        userScore.textContent = userScoreNum;
-        computerScore.textContent = computerScoreNum;
-        result.textContent = result;
-        actionMessage.textContent = `${userChoice} vs ${computerChoice}. ${result}`;
-    }
-
-    // Event listener for the players choices.
-    function startGame() {
-        choices.forEach((choice) => {
-            choice.addEventListener("click", function () {
-                const userChoice = this.id;
-                const computerChoice = getComputerChoice();
-                const result = whoWins(userChoice, computerChoice);
-                updateScore(userChoice, computerChoice, result);
-            });
-        });
-    }
-
-    //Starts the game.
-    startGame();
-
+  //Starts the game.
+  startGame();
 });
